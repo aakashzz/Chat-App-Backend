2db08307b6a808fbcf4d2e55c6a00d8c68bfeaf0
Aakash malviya <malviyaaakash4509@gmail.com>
2024-10-20 01:22:51 +0530

    more problem i face that userProfies and Socket Not Receving correct user

--- src/server.js
+++ src/server.js
@@ -53,28 +53,15 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-   console.log("a user connected");
-   socket.on("setup", (userDetails) => {
-      socket.join(userDetails._id);
-      console.log(userDetails._id);
-      socket.emit("connected");
-   });
+   console.log("a user connected",socket.id);
+   // socket.on("setup", (userDetails) => {
+   //    socket.join(userDetails[0]._id);
+   //    // socket.emit("connected");
+   // });
 
-   //join to chat
-   socket.on("join chat",(room)=>{
-      socket.join(room)
-      console.log("User Room id: ",room)
-   })
-
-   socket.on("new message",(newMessageReceived)=>{
-      var chat = newMessageReceived.chat;
-
-      if(!chat.user) return console.error('chat.user not defined');
-
-      chat.user.forEach(user => {
-         if(user._id === newMessageReceived.sendedBy._id) return;
-         socket.in(user._id).emit("message received", newMessageReceived)
-      });
+   socket.on("sendMessage",(newMessageReceived)=>{
+      console.log(newMessageReceived)
+      socket.to(newMessageReceived.data.data.sendedBy).emit('receiveMessage', newMessageReceived);
    }) 
 });
 
